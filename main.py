8426e1deb166a1691f83ea5e34e3aba3be470d58
hindsighttt <131590753+hindsighttt@users.noreply.github.com>
2023-04-24 14:29:23 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from random import *
+
+solution = int(randrange(1,11))
+
+def game():
+    print("Guess a number between 1 and 10")
+    while solution != 12:
+        userguess = int(input("Enter your guess!: "))
+        if solution == userguess:
+            print("You guessed the right answer which was " +str(solution))
+            break
+        else:
+            print("You guessed the wrong solution :(")
+            print("Try again!")
+            pass
+game()
